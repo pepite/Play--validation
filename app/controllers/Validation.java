15c6a750be917e2f8e4ef7609da8fa374221dcab
Nicolas Leroux <nicolas@lunatech.com>
2010-03-19 22:17:37 +0100

    initial release

---
+++ app/controllers/Validation.java
@@ -0,0 +1,117 @@
+package controllers;
+
+import net.sf.oval.ConstraintViolation;
+import net.sf.oval.context.MethodParameterContext;
+import net.sf.oval.guard.Guard;
+import play.Logger;
+import play.data.validation.Error;
+import play.exceptions.ActionNotFoundException;
+import play.i18n.Messages;
+import play.mvc.ActionInvoker;
+import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.Router;
+import play.utils.Java;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+import play.data.validation.Error;
+
+public class Validation extends Controller {
+
+    // TODO: manage bla.bli.property
+    public static void validate(String formAction, String name, String value) throws Exception {
+        final Http.Request request = new Http.Request();
+        request.path = formAction;
+        request.method = "POST";
+
+        // The route method modify the request object.
+        final Router.Route route = Router.route(request);
+        final Object[] ca = ActionInvoker.getActionMethod(request.action);
+        final Method actionMethod = (Method) ca[1];
+
+        final String[] paramsNames = Java.parameterNames(actionMethod);
+        String className = name;
+        String f = name;
+        // It should be only one parameters
+        int pos = -1;
+        for (int i = 0; i < paramsNames.length; i++) {
+            String arg = paramsNames[i];
+            if (name.indexOf(".") > 0) {
+                className = name.substring(0, name.indexOf("."));
+            }
+            if (name.startsWith(arg + ".") && className.equals(arg)) {
+                pos = i;
+                f = name.substring(name.lastIndexOf(".") + 1);
+                break;
+            } else if (className.equals(arg)) {
+                pos = i;
+                f = className;
+                break;
+            }
+
+        }
+
+        if (pos != -1) {
+            final Class type = actionMethod.getParameterTypes()[pos];
+            Object validatedInstance = type.newInstance();
+            if (f.equals(className)) {
+                validatedInstance = value;
+                for (Annotation[] annotations : actionMethod.getParameterAnnotations()) {
+                    if (annotations.length > 0) {
+                        List<ConstraintViolation> violations = new Validator().validateAction(actionMethod);
+                        ArrayList<Error> errors = new ArrayList<Error>();
+                        String[] paramNames = Java.parameterNames(actionMethod);
+                        for (ConstraintViolation violation : violations) {
+                            // Use a json array instead...
+                            renderText(message(paramNames[((MethodParameterContext) violation.getContext()).getParameterIndex()], violation.getMessage(), violation.getMessageVariables() == null ? new String[0] : violation.getMessageVariables().values().toArray(new String[0])));
+                        }
+                        break;
+                    }
+                }
+            } else {
+                type.getDeclaredField(f).set(validatedInstance, value);
+
+                final play.data.validation.Validation.ValidationResult result = validation.valid(validatedInstance);
+                final List<play.data.validation.Error> errors = validation.errorsMap().get("validatedInstance." + f);
+                if (errors != null && errors.size() > 0) {
+                    // Use a json array instead...
+                    renderText(errors.get(0).message());
+                }
+            }
+        }
+
+        renderText("");
+    }
+
+     private static String message(String key, String message, String[] variables) {
+        key = Messages.get(key);
+        Object[] args = new Object[variables.length + 1];
+        System.arraycopy(variables, 0, args, 1, variables.length);
+        args[0] = key;
+        return Messages.get(message, (Object[])args);
+    }
+
+     static class Validator extends Guard {
+
+        public List<ConstraintViolation> validateAction(Method actionMethod) throws Exception {
+            List<ConstraintViolation> violations = new ArrayList<ConstraintViolation>();
+            Object instance = null;
+            // Patch for scala defaults
+            if(!Modifier.isStatic(actionMethod.getModifiers()) && actionMethod.getDeclaringClass().getSimpleName().endsWith("$")) {
+                try {
+                    instance = actionMethod.getDeclaringClass().getDeclaredField("MODULE$").get(null);
+                } catch(Exception e) {
+                    throw new ActionNotFoundException(Http.Request.current().action, e);
+                }
+            }
+            Object[] rArgs = ActionInvoker.getActionMethodArgs(actionMethod, instance);
+            validateMethodParameters(null, actionMethod, rArgs, violations);
+            validateMethodPre(null, actionMethod, rArgs, violations);
+            return violations;
+        }
+    }
+}
